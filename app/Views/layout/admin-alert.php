798aee331f48f38fe8591bbc3716ac23a93c67c1
AndhikaK <andhika.smandading@gmail.com>
2021-03-14 17:38:03 +0600

    fix admin-alert

--- app/Views/layout/admin-alert.php
+++ app/Views/layout/admin-alert.php
@@ -1,7 +1,7 @@
 <!-- success alert -->
 <?php if (session()->getFlashData('success-delete')) : ?>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Hapus data berhasil!</strong> <?= session()->getFlashData('success-edit') ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <span class="badge badge-pill badge-success p-2">Berhasil!</span> <?= session()->getFlashData('success-delete') ?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -9,8 +9,8 @@
 <?php endif; ?>
 
 <?php if (session()->getFlashData('success-add')) : ?>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Tambah data berhasil!</strong> <?= session()->getFlashData('success-edit') ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <span class="badge badge-pill badge-success p-2">Berhasil!</span> <?= session()->getFlashData('success-add') ?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -18,8 +18,8 @@
 <?php endif; ?>
 
 <?php if (session()->getFlashData('success-edit')) : ?>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Edit berhasil!</strong> <?= session()->getFlashData('success-edit') ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <span class="badge badge-pill badge-success p-2">Berhasil!</span> <?= session()->getFlashData('success-edit') ?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -31,8 +31,8 @@
 
 <!-- failded alert -->
 <?php if (session()->getFlashData('failed-delete')) : ?>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Hapus data gagal!</strong> <?= session()->getFlashData('failed-delete') ?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <span class="badge badge-pill badge-danger p-2">Gagal!</span> <?= session()->getFlashData('failed-delete') ?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -40,8 +40,8 @@
 <?php endif; ?>
 
 <?php if (session()->getFlashData('failed-add')) : ?>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Tambah data gagal!</strong> <?= session()->getFlashData('failed-add') ?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <span class="badge badge-pill badge-danger p-2">Gagal!</span> <?= session()->getFlashData('failed-add') ?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -49,8 +49,8 @@
 <?php endif; ?>
 
 <?php if (session()->getFlashData('failed-edit')) : ?>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Edit data gagal!</strong> <?= session()->getFlashData('failed-edit') ?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <span class="badge badge-pill badge-danger p-2">Gagal!</span> <?= session()->getFlashData('failed-edit') ?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
